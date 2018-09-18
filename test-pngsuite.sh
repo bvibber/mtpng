bd08e8c236cd49e4e1b2b33a858c7876c762a21b
Brion Vibber <brion@pobox.com>
2018-09-18 14:22:39 -0700

    Add some of the basic PngSuite files
    For testing color depths. Lots fail now. :D

---
+++ test-pngsuite.sh
@@ -0,0 +1,12 @@
+# Run all the pngsuite files at given options
+# reads input from pngsuite/*.png
+# creates output in out/*.png
+
+mkdir -p out && \
+cd pngsuite && \
+for x in *.png
+do
+  cargo --quiet run --release -- "$@" "$x" "../out/$x"
+  #|| exit 1
+  echo ""
+done
