e06ce55e7bd019dea3fa8efe76174cb983fac18e
Brooke <bvibber@pobox.com>
2026-06-08 13:04:30 -0700

    Rework C API builder for windows
    build-win.bat refactored a bit:
    
    * now builds for all three target arches under build/%target%
    * doesn't attempt to run the sample for you
    * avoid seeing vars like CC and CFLAGS/LDFLAGS that confuse cargo

--- build-win.bat
+++ build-win.bat
@@ -4,26 +4,11 @@ setlocal
 rem C API build script for Windows.
 rem Must be run from a Visual Studio Dev Tools console window
 
-if "x%1"=="xx86" (
-    set arch=x86
-    set hostarch=x86
-    set target=i686-pc-windows-msvc
-    shift
-) else if "x%1"=="xx64" (
-    set arch=x64
-    set hostarch=x64
-    set target=x86_64-pc-windows-msvc
-    shift
-) else if "x%1"=="xarm64" (
-    set arch=arm64
-    set hostarch=x86
-    set target=aarch64-pc-windows-msvc
-    shift
-) else (
-    set arch=x64
-    set hostarch=x64
-    set target=x86_64-pc-windows-msvc
-)
+goto :Main
+
+
+
+:BuildArch
 
 set CARGO=cargo
 set PROFILE=release
@@ -31,26 +16,42 @@ set RUSTLIBDIR=target\%target%\%PROFILE%
 
 set SOURCES=c\sample.c
 set HEADERS=c\mtpng.h
-set EXE=build\sample.exe
+set EXE=build\%target%\sample.exe
+if not exist build mkdir build
+if not exist build\%target% mkdir build\%target%
+if not exist build\%target%\out mkdir build\%target%\out
+
+rustup target add %target%
 
 %CARGO% build --target=%target% --release --features capi
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-if not exist build mkdir build
-copy %RUSTLIBDIR%\mtpng.dll.lib build\mtpng.lib
-copy %RUSTLIBDIR%\mtpng.dll build\mtpng.dll
-
+copy %RUSTLIBDIR%\mtpng.dll.lib build\%target%\mtpng.lib
+copy %RUSTLIBDIR%\mtpng.dll build\%target%\mtpng.dll
 
 rem Now set up and build our C app!
 call VsDevCmd -arch=%arch% -host_arch=%hostarch%
 
-rem This is all hacky and not gonna help with cross-compiling. :D
-rem Warning: do not set CC before calling cargo! It will explode.
-set CC=cl
-set CFLAGS=
-set LDFLAGS=build\mtpng.lib
-%CC% %CFLAGS% /Fe%EXE% %SOURCES%  %LDFLAGS% /link
+cl /Fe%EXE% %SOURCES% build\%target%\mtpng.lib /link
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-if not exist out mkdir out
-%CDYLIB_PATHVAR% .\%EXE%
+exit /b
+
+
+
+:Main
+
+set arch=x86
+set hostarch=x64
+set target=i686-pc-windows-msvc
+call :BuildArch
+
+set arch=x64
+set hostarch=x64
+set target=x86_64-pc-windows-msvc
+call :BuildArch
+
+set arch=arm64
+set hostarch=x64
+set target=aarch64-pc-windows-msvc
+call :BuildArch
