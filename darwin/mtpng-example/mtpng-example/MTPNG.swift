56e0352c7d88357bdb8d602b41e1d721ca6ffac7
Brooke Vibber <bvibber@pobox.com>
2026-06-15 13:10:37 -0700

    wip

---
+++ darwin/mtpng-example/mtpng-example/MTPNG.swift
@@ -0,0 +1,206 @@
+//
+//  MTPNG.swift
+//  mtpng-example
+//
+//  Created by Brooke on 6/15/26.
+//  Copyright © 2026 Brooke Vibber. All rights reserved.
+//
+
+import Foundation
+import mtpng
+
+enum MTPNGError: Error {
+    case unknownError
+    case inputOutOfBounds
+}
+
+enum MTPNGColor: UInt32 {
+    case Greyscale = 0 // MTPNG_COLOR_GREYSCALE
+    case Truecolor = 2 // MTPNG_COLOR_TRUECOLOR
+    case IndexedColor = 3 // MTPNG_COLOR_INDEXED_COLOR
+    case GreyscaleAlpha = 4 // MTPNG_COLOR_GREYSCALE_ALPHA
+    case TruecolorAlpha = 6 // MTPNG_COLOR_TRUECOLOR_ALPHA
+}
+
+typealias MTPNGWriteFunc = (_: Span<UInt8>) -> Int
+
+typealias MTPNGFlushFunc = () -> Bool
+
+class MTPNGThreadPool {
+    var pool: OpaquePointer? = nil;
+
+    init(threads: Int) throws {
+        let ret = mtpng_threadpool_new(&self.pool, threads);
+        if (ret != MTPNG_RESULT_OK) {
+            throw MTPNGError.unknownError
+        }
+        if self.pool == nil {
+            throw MTPNGError.unknownError
+        }
+    }
+    
+    deinit {
+        let ret = mtpng_threadpool_release(&pool);
+        if (ret != MTPNG_RESULT_OK) {
+            NSLog("Unexpected failure in mtpng_threadpool_release");
+        }
+        if self.pool != nil {
+            NSLog("Invalid state after mtpng_threadpool_release");
+        }
+    }
+}
+
+class MTPNGEncoderOptions {
+
+    var options: OpaquePointer? = nil;
+
+    init() throws {
+        let ret = mtpng_encoder_options_new(&self.options);
+        if ret != MTPNG_RESULT_OK {
+            throw MTPNGError.unknownError
+        }
+        if self.options == nil {
+            throw MTPNGError.unknownError
+        }
+    }
+
+    deinit {
+        let ret = mtpng_encoder_options_release(&self.options);
+        if (ret != MTPNG_RESULT_OK) {
+            NSLog("Unexpected failure in mtpng_encoder_options_release");
+        }
+        if self.options != nil {
+            NSLog("Invalid state after mtpng_encoder_options_release");
+        }
+
+    }
+    
+    func setThreadPool(pool: MTPNGThreadPool) throws {
+        let ret = mtpng_encoder_options_set_thread_pool(self.options, pool.pool);
+        if (ret != MTPNG_RESULT_OK) {
+            throw MTPNGError.unknownError;
+        }
+    }
+
+    // mtpng_encoder_options_set_filter
+    // mtpng_encoder_options_set_strategy
+    // mtpng_encoder_options_set_compression_level
+    // mtpng_encoder_options_set_chunk_size
+}
+
+class MTPNGHeader {
+    var header: OpaquePointer? = nil;
+    
+    init () throws {
+        let ret = mtpng_header_new(&self.header);
+        if ret != MTPNG_RESULT_OK {
+            throw MTPNGError.unknownError;
+        }
+        if self.header == nil {
+            throw MTPNGError.unknownError
+        }
+    }
+
+    deinit {
+        let ret = mtpng_header_release(&self.header);
+        if (ret != MTPNG_RESULT_OK) {
+            NSLog("Unexpected failure in mtpng_header_release!");
+        }
+        if self.header != nil {
+            NSLog("Invalid state after mtpng_header_release!");
+        }
+    }
+    
+    func setSize(width: UInt32, height: UInt32) throws {
+        let ret = mtpng_header_set_size(self.header, width, height);
+        if (ret != MTPNG_RESULT_OK) {
+            throw MTPNGError.inputOutOfBounds;
+        }
+    }
+
+    func setColor(color: MTPNGColor, bits: UInt8) throws {
+        let ret = mtpng_header_set_color(self.header, mtpng_color_t(color.rawValue), bits);
+        if (ret != MTPNG_RESULT_OK) {
+            throw MTPNGError.unknownError;
+        }
+    }
+
+
+}
+
+func write_func(_user_data: UnsafeMutableRawPointer?, noisolation _bytes: UnsafePointer<UInt8>?, noisolation _len: Int) -> Int
+{
+    let myself = Unmanaged<MTPNGEncoder>.fromOpaque(_user_data!).takeUnretainedValue();
+    if let writeFunc = myself.writeFunc {
+        if _bytes == nil {
+            return 0;
+        }
+        nonisolated(unsafe) let bytes = UnsafeBufferPointer.init(start: _bytes, count: _len);
+        return writeFunc(bytes.span);
+    } else {
+        return _len;
+    }
+}
+
+func flush_func(_user_data: UnsafeMutableRawPointer?) -> Bool
+{
+    let myself = Unmanaged<MTPNGEncoder>.fromOpaque(_user_data!).takeUnretainedValue();
+    if let flushFunc = myself.flushFunc {
+        return flushFunc();
+    } else {
+        return true;
+    }
+}
+
+class MTPNGEncoder {
+    var encoder: OpaquePointer? = nil;
+    var writeFunc: MTPNGWriteFunc? = nil;
+    var flushFunc: MTPNGFlushFunc? = nil;
+
+    init (write: MTPNGWriteFunc?, flush: MTPNGFlushFunc?, options: MTPNGEncoderOptions) throws {
+        self.writeFunc = write;
+        self.flushFunc = flush;
+        let user_data = UnsafeMutableRawPointer.init(Unmanaged.passUnretained(self).toOpaque());
+        let ret = mtpng_encoder_new(&encoder,
+                          write_func,
+                          flush_func,
+                          user_data,
+                          options.options);
+        if ret != MTPNG_RESULT_OK {
+            throw MTPNGError.unknownError;
+        }
+    }
+    
+    deinit {
+        let ret = mtpng_encoder_release(&self.encoder);
+        if ret != MTPNG_RESULT_OK {
+            NSLog("Unexpected failure in mtpng_encoder_release");
+        }
+        if self.encoder != nil {
+            NSLog("Invalid state after mtpng_encoder_release");
+        }
+    }
+    
+    func writeHeader(header: MTPNGHeader) throws {
+        let ret = mtpng_encoder_write_header(self.encoder, header.header);
+        if ret != MTPNG_RESULT_OK {
+            throw MTPNGError.unknownError;
+        }
+    }
+    
+    func writeImageRows(bytes: Span<UInt8>) throws {
+        try bytes.withUnsafeBufferPointer { (buffer: UnsafeBufferPointer) in
+            let ret = mtpng_encoder_write_image_rows(encoder, buffer.baseAddress, buffer.count);
+            if ret != MTPNG_RESULT_OK {
+                throw MTPNGError.unknownError;
+            }
+        }
+    }
+    
+    func finish() throws {
+        let ret = mtpng_encoder_finish(&encoder);
+        if (ret != MTPNG_RESULT_OK) {
+            throw MTPNGError.unknownError;
+        }
+    }
+}
--- darwin/mtpng-example/mtpng-example/ViewController.swift
+++ darwin/mtpng-example/mtpng-example/ViewController.swift
@@ -7,27 +7,6 @@
 //
 
 import UIKit
-import mtpng
-
-func write_func(_user_data: UnsafeMutableRawPointer?, noisolation _bytes: UnsafePointer<UInt8>?, noisolation _len: Int) -> Int
-{
-    let myself = Unmanaged<ViewController>.fromOpaque(_user_data!).takeUnretainedValue();
-    if _bytes == nil {
-        return 0;
-    }
-    nonisolated(unsafe) let bytes = UnsafeBufferPointer.init(start: _bytes, count: _len);
-    return MainActor.assumeIsolated {
-        return myself.writeFunc(bytes: bytes);
-    }
-}
-
-func flush_func(_user_data: UnsafeMutableRawPointer?) -> Bool
-{
-    let myself = Unmanaged<ViewController>.fromOpaque(_user_data!).takeUnretainedValue();
-    return MainActor.assumeIsolated {
-        return myself.flushFunc();
-    }
-}
 
 class ViewController: UIViewController {
 
@@ -36,9 +15,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var samplePicker: UISegmentedControl!
     @IBOutlet weak var compressButton: UIButton!
     @IBOutlet weak var timeLabel: UILabel!
-    
-    var buffer: [UInt8] = [];
-   
 
     public func writeFunc(bytes: any Collection<UInt8>) -> Int {
         // fake output
@@ -50,7 +26,7 @@ class ViewController: UIViewController {
     }
 
 
-    func savePngImage(image: UIImage, threads: Int) {
+    func savePngImage(image: UIImage, threads: Int) -> TimeInterval {
         // Draw the UIImage into a CGImage with specified RGB order
         let cgi = image.cgImage!;
         
@@ -69,80 +45,45 @@ class ViewController: UIViewController {
         context.draw(cgi, in: CGRect(x: 0, y: 0, width: width, height: height));
         
         // And get the data out.
-        let data = context.data!.assumingMemoryBound(to: UInt8.self);
+        let inputPointer = context.data!.assumingMemoryBound(to: UInt8.self);
+        let inputBuffer = UnsafeBufferPointer(start: inputPointer, count: width * height * stride);
+        let data = inputBuffer.span;
 
-        // Create a manual thread pool
-        var pool = OpaquePointer.init(bitPattern: 0);
-        let user_data = UnsafeMutableRawPointer.init(Unmanaged.passUnretained(self).toOpaque());
+        do {
+            // Create a manual thread pool
+            let pool = try MTPNGThreadPool.init(threads: threads);
 
-        var ret = mtpng_threadpool_new(&pool, threads);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
+            let options = try MTPNGEncoderOptions.init();
+            try options.setThreadPool(pool: pool);
 
-        // Set it in the options
-        var options = OpaquePointer.init(bitPattern: 0);
-        ret = mtpng_encoder_options_new(&options);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
+            let start = Date();
 
-        ret = mtpng_encoder_options_set_thread_pool(options, pool);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
+            // Create the encoder
+            var outputBuffer: [UInt8] = [];
+            var encoder = try MTPNGEncoder.init(
+                write: { (bytes: Span<UInt8>) -> Int in
+                    bytes.withUnsafeBufferPointer { buffer in
+                        outputBuffer.append(contentsOf: buffer)
+                    }
+                    return bytes.count;
+                },
+                flush: nil,
+                options: options);
 
-        // Create the encoder
-        var encoder = OpaquePointer.init(bitPattern: 0);
-        ret = mtpng_encoder_new(&encoder,
-                          write_func,
-                          flush_func,
-                          user_data,
-                          options);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
+            var header = try MTPNGHeader.init();
+            try header.setSize(width: UInt32(width), height: UInt32(height));
+            try header.setColor(color: MTPNGColor.Truecolor, bits: 8);
+            try encoder.writeHeader(header: header);
 
-        var header = OpaquePointer.init(bitPattern: 0);
-        ret = mtpng_header_new(&header);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
+            try encoder.writeImageRows(bytes: data);
+            try encoder.finish();
 
-        ret = mtpng_header_set_size(header, UInt32(width), UInt32(height));
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
+            let delta = Date().timeIntervalSince(start);
 
-        ret = mtpng_header_set_color(header, MTPNG_COLOR_TRUECOLOR_ALPHA, 8);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
-
-        ret = mtpng_encoder_write_header(encoder, header);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
-
-        ret = mtpng_encoder_write_image_rows(encoder, &(data[0]), stride * height);
-        ret = mtpng_encoder_finish(&encoder);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
-
-        ret = mtpng_header_release(&header);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
-
-        ret = mtpng_encoder_options_release(&options);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
-        }
-
-        ret = mtpng_threadpool_release(&pool);
-        if (ret != MTPNG_RESULT_OK) {
-            NSLog("Failure!");
+            return delta;
+        } catch {
+            print("Unexpected error: \(error).")
+            return 0;
         }
     }
     
@@ -168,9 +109,7 @@ class ViewController: UIViewController {
         self.timeLabel.text = "Loading...";
         let image = UIImage.init(named: self.samplePicker.titleForSegment(at: self.samplePicker.selectedSegmentIndex)!)!;
         self.timeLabel.text = "Running...";
-        let start = Date();
-        savePngImage(image: image, threads: Int(threadSlider.value));
-        let delta = Date().timeIntervalSince(start);
+        let delta = savePngImage(image: image, threads: Int(threadSlider.value));
         let ms = Int(delta * 1000.0);
         self.timeLabel.text = String(format: "Done in %d ms.", ms);
     }
